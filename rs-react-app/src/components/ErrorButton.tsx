1dce6b4c8b485a321def816965b88ac5a2be0ba9
Elena Kovalevskaya <elena.kovalevskaya6@gmail.com>
2025-02-03 00:56:36 +0300

    feat: create Error Boundary component

--- rs-react-app/src/components/ErrorButton.tsx
+++ rs-react-app/src/components/ErrorButton.tsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './errorbutton.css';
 
 export class ErrorButton extends Component {
+  infos = [];
+  state = { infoIndex: undefined };
+
+  onClick = () => {
+    this.setState({ infoIndex: 1 });
+  };
+
   render() {
+    const info = this.state.infoIndex
+      ? this.infos[this.state.infoIndex].toLowerCase()
+      : '';
     return (
-      <button className="error-button" type="button">
-        Error button
+      <button className="error-button" type="button" onClick={this.onClick}>
+        {`Error button${info}`}
       </button>
     );
   }
--- rs-react-app/src/components/Search.tsx
+++ rs-react-app/src/components/Search.tsx
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './search.css';
 
 export class Search extends Component {
+  state = { value: '' };
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+    console.log(this.state.value);
+  };
   render() {
     return (
       <div className="search-container">
-        <input className="search-input" placeholder="search something..." />
+        <input
+          className="search-input"
+          placeholder="search something..."
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
         <button className="search-button" type="button">
           Search
         </button>
