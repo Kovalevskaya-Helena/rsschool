f076ca259fbc8065fbe1d957b90264f0bf3b907c
Elena Kovalevskaya <elena.kovalevskaya6@gmail.com>
2025-02-02 22:54:25 +0300

    feat: creat ListItem layout

---
+++ rs-react-app/src/components/ErrorButton.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class ErrorButton extends Component {
+  render() {
+    return <button type="button">Error button</button>;
+  }
+}
---
+++ rs-react-app/src/components/ListItem.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export class ListItem extends Component {
+  render() {
+    const { label } = this.props;
+    return <span>label</span>;
+  }
+}
