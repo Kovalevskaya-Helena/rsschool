5186beb5ab87d14fd115bc287a9a06bc6c59fa24
Elena Kovalevskaya <elena.kovalevskaya6@gmail.com>
2025-02-08 16:48:05 +0300

    refactor: adjust project structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,82 +1,10 @@
-import { useState, useEffect } from 'react';
-import { Header } from '../Header';
-import { Main } from '../Main/Main';
-import { ErrorButton } from '../ErrorButton';
-import { ErrorBoundary } from '../ErrorBoudary';
-import { requests } from '../../helpers/requests';
-import { Items, LoadStatus } from '../../helpers/types';
-import './app.css';
-
-const getInitialSearchText = () => localStorage.getItem('label') ?? '';
-
-interface ItemsLoadState {
-  items: Items[];
-  loadStatus: LoadStatus;
-  errorText: string;
-}
+import { ErrorBoundary } from '../ErrorBoundary';
+import { PeopleSearch } from '../PeopleSearch';
 
 export const App = () => {
-  const [searchText, setSearchText] = useState<string>(getInitialSearchText);
-  const [itemsLoadState, setItemsLoadState] = useState<ItemsLoadState>({
-    items: [],
-    loadStatus: 'pending',
-    errorText: '',
-  });
-
-  useEffect(() => {
-    getPeoples();
-  }, []);
-
-  const setCacheToLocalStorage = () => {
-    localStorage.setItem('label', searchText.trim());
-  };
-
-  const getPeoples = async () => {
-    const peopleUrl = new URL('https://swapi.dev/api/people/');
-    peopleUrl.searchParams.set('search', searchText);
-
-    setItemsLoadState({
-      items: [],
-      loadStatus: 'loading',
-      errorText: '',
-    });
-
-    const {
-      data: { results: items },
-      error: errorText,
-    } = await requests.get(peopleUrl);
-
-    setItemsLoadState({
-      items,
-      loadStatus: errorText ? 'error' : 'loaded',
-      errorText,
-    });
-  };
-
-  const onChangeSearch = (text: string) => {
-    setSearchText(text);
-  };
-
-  const onSearchStart = () => {
-    setCacheToLocalStorage();
-    getPeoples();
-  };
-
   return (
     <ErrorBoundary>
-      <div className="container">
-        <Header
-          searchText={searchText}
-          onChangeText={onChangeSearch}
-          onSearch={onSearchStart}
-        />
-        <Main
-          people={itemsLoadState.items}
-          loadStatus={itemsLoadState.loadStatus}
-          errorText={itemsLoadState.errorText}
-        />
-        <ErrorButton />
-      </div>
+      <PeopleSearch />
     </ErrorBoundary>
   );
 };
---
+++ src/components/PeopleSearch/PeopleSearch.tsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react';
+import { Header } from '../Header';
+import { Main } from '../Main/Main';
+import { ErrorButton } from '../ErrorButton';
+import { requests } from '../../helpers/requests';
+import { Items, LoadStatus } from '../../helpers/types';
+import './peopleSearch.css';
+
+const getInitialSearchText = () => localStorage.getItem('label') ?? '';
+
+interface ItemsLoadState {
+  items: Items[];
+  loadStatus: LoadStatus;
+  errorText: string;
+}
+
+export const PeopleSearch = () => {
+  const [searchText, setSearchText] = useState<string>(getInitialSearchText);
+  const [itemsLoadState, setItemsLoadState] = useState<ItemsLoadState>({
+    items: [],
+    loadStatus: 'pending',
+    errorText: '',
+  });
+
+  useEffect(() => {
+    getPeoples();
+  }, []);
+
+  const setCacheToLocalStorage = () => {
+    localStorage.setItem('label', searchText.trim());
+  };
+
+  const getPeoples = async () => {
+    const peopleUrl = new URL('https://swapi.dev/api/people/');
+    peopleUrl.searchParams.set('search', searchText);
+
+    setItemsLoadState({
+      items: [],
+      loadStatus: 'loading',
+      errorText: '',
+    });
+
+    const {
+      data: { results: items },
+      error: errorText,
+    } = await requests.get(peopleUrl);
+
+    setItemsLoadState({
+      items,
+      loadStatus: errorText ? 'error' : 'loaded',
+      errorText,
+    });
+  };
+
+  const onChangeSearch = (text: string) => {
+    setSearchText(text);
+  };
+
+  const onSearchStart = () => {
+    setCacheToLocalStorage();
+    getPeoples();
+  };
+
+  return (
+    <div className="container">
+      <Header
+        searchText={searchText}
+        onChangeText={onChangeSearch}
+        onSearch={onSearchStart}
+      />
+      <Main
+        people={itemsLoadState.items}
+        loadStatus={itemsLoadState.loadStatus}
+        errorText={itemsLoadState.errorText}
+      />
+      <ErrorButton />
+    </div>
+  );
+};
