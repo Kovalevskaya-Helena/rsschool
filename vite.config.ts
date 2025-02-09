06973aa1e9435346d514b0ded0fa7ef18a322c96
Elena Kovalevskaya <elena.kovalevskaya6@gmail.com>
2025-02-10 01:03:26 +0300

    fix: fix deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: '',
+  base: '/',
   test: {
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['./setupTest.ts'],
   },
-});
+}));
